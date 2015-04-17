05d8a65c311a10ffd7b9e294159bbe4b271fba52
Martin Cermak <martin.cermak@gooddata.com>
2015-04-17 15:20:37 +0200

    Basic structure

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,12 +1,30 @@
 import { Component } from 'react';
 
+import { Menu } from '../components/menu';
+
+import { Hello } from '../components/hello';
+import { About } from '../components/about';
+import { Schedule } from '../components/schedule';
+import { Social } from '../components/social';
+import { Projects } from '../components/projects';
+import { Awards } from '../components/awards';
+
 export default class Index extends Component {
     displayName: 'Index'
 
     render() {
         return (
             <div>
-                index
+                <Menu />
+
+                <!-- pages -->
+
+                <Hello />
+                <About />
+                <Schedule />
+                <Social />
+                <Projects />
+                <Awards />
             </div>
         );
     }
