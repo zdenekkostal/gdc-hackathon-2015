cdfb3e0ac75788bde7f6817ca205856ceb68a210
Martin Cermak <martin.cermak@gooddata.com>
2015-04-18 15:51:52 +0200

    All triangles

--- app/components/triangles.jsx
+++ app/components/triangles.jsx
@@ -6,14 +6,30 @@ export default class Triangles extends Component {
     render() {
         return (
             <div id="triangles">
-                <div className="tri-g-1"></div>
+                <div className="triangles-left">
+                    <div className="tri-g-1"></div>
 
-                <div className="tri-1"></div>
-                <div className="tri-2"></div>
+                    <div className="tri-l-1"></div>
+                    <div className="tri-l-2"></div>
 
-                <div className="tri-p-1"></div>
-                <div className="tri-p-2"></div>
-                <div className="tri-p-3"></div>
+                    <div className="tri-p-1"></div>
+                    <div className="tri-p-2"></div>
+                    <div className="tri-p-3"></div>
+                    <div className="tri-p-4"></div>
+
+                    <div className="tri-g-2"></div>
+                </div>
+
+                <div className="triangles-right">
+                    <div className="tri-r-3"></div>
+                    <div className="tri-r-big"></div>
+
+                    <div className="tri-r-1"></div>
+                    <div className="tri-r-2"></div>
+
+                    <div className="tri-r-4"></div>
+                    <div className="tri-r-5"></div>
+                </div>
 
             </div>
         );
