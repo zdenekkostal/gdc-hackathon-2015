ca83d6bfeeb7d2552bd0c0516dc4af0fa243f7ad
Zdenek Kostal <zdenek.kostal@gooddata.com>
2015-05-07 15:40:36 +0200

    Downgrade

--- server/web.js
+++ server/web.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var path = require('path');
 var repo = require('./repo.js');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var serveStatic = require('serve-static');
 
 var app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(express.logger());
+app.use(express.bodyParser());
 app.use(serveStatic(path.join(__dirname, '../build')));
 
 app.get('/repo', repo.findAll);
