a9aa502893e62ab70eb08d9e199a8ce2f914b795
Martin Cermak <martin.cermak@gooddata.com>
2015-05-07 09:46:36 +0200

    Awards

--- app/components/awards.jsx
+++ app/components/awards.jsx
@@ -6,7 +6,13 @@ export default class Awards extends Component {
     render() {
         return (
             <div id="awards">
-                Awards
+                <h2>Awards</h2>
+
+                <p>Every team that want to compete to win "Lego Bear" <strong>has to record the 60 seconds video</strong> that introduces the project and send the link to this video to hackathon@gooddata.com with short description of the team (team members, etc).</p>
+
+                <p>Deadline for delivering the video recording is <strong>Friday 6pm</strong> (both PST and CET). The jury will choose three finalist during the weekend and the winner will be announced the following Monday, 18th May on the CZ - US All Hands Ceremony.</p>
+
+                <img src="app/images/bear.jpg" />
             </div>
         );
     }
